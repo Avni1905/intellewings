88ad601a0c2c598c241ca3bbff8e4881402deefe
Avni1905 <avni19may@gmail.com>
2024-12-16 22:55:26 +0530

    test

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/contacts",
+    baseURL: "https://intellewings-assignment-pobb.onrender.com/api/contacts",
 });
 
 export const fetchContacts = async() => api.get("/");
